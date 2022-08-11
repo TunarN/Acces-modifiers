f83c79dfe2694540e9b33f9127a63dca64b0fa1a
TunarN <tunarzn@code.edu.az>
2022-08-11 17:47:03 +0400

    Class

---
+++ Classtask/Program.cs
@@ -0,0 +1,17 @@
+﻿using Classtask.metod;
+using System;
+
+namespace Classtask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User Lemusha = new User("Lemusha");
+            Console.WriteLine("Lemusha");
+            Console.WriteLine("Password");
+            Lemusha.Password = Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Classtask/metod/User.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classtask.metod
+{
+    class User
+    {
+        private string _username;
+        private string _password;
+        public User(string Username)
+        {
+            this._username = Username;   
+        }
+        public string UserName
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                if (value.Length >= 6 && value.Length <= 25)
+                {
+                    _username = value;
+                }
+            }
+
+
+        }
+        public string Password {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (value.Length>=8 && value.Length<=25 && HasDigit(value)&&HasUpper(value)&&HasLower(value))
+                {
+                    _password = value;
+                }
+            }
+        }
+        private bool HasDigit (string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i]>47 && word[i]<58)
+                {
+                    return true;
+                }
+                
+            }
+            return false;
+        }
+        private bool HasUpper(string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i]>64 && word[i]<91)
+                {
+                    return true;
+                }
+               
+            }
+            return false;
+
+        }
+        private bool HasLower(string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i]>96 && word[i]<123)
+                {
+                    return true;
+                }
+                
+            }
+            return false; 
+
+        }
+
+    }
+}
+
